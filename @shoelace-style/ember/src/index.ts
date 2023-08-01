428c75de035dea2add6979bb0d12f2a986cf8e0c
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-08-01 12:34:14 -0400

    That's better

--- @shoelace-style/ember/src/index.ts
+++ @shoelace-style/ember/src/index.ts
@@ -1,2 +1,10 @@
-import '@shoelace-style/shoelace/dist/themes/light.css';
-import '@shoelace-style/shoelace/dist/themes/dark.css';
+// @ts-ignore
+import light from '@shoelace-style/shoelace/dist/themes/light.css';
+// @ts-ignore
+import dark from '@shoelace-style/shoelace/dist/themes/dark.css';
+
+export function _() {
+  // this is weird, but we need something to keep these references so they don't get optimized away.
+  // I would classify this as a bug in embroider / ember-auto-import or the blueprint
+  console.log(light, dark);
+}
